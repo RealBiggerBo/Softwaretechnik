660d7a6d8af603c585eec3b3070c3d88696ff4d1
Natalie Berndt <natalieberndt@Natalies-Air.fritz.box>
2025-12-09 10:21:43 +0100

    update navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import LoginPage from "./pages/LoginPage";
-/*import DataviewPage from "./pages/DataviewPage";
-import MainPage from "./pages/MainPage";
-import SearchPage from "./pages/SearchPage";
+import MainPage from './pages/MainPage';
+/*import DataviewPage from "./pages/DataviewPage";*/
+/*import SearchPage from "./pages/SearchPage";
 import SettingsPage from "./pages/SettingsPage";
 import StatisticsPage from "./pages/StatisticsPage";*/
 
@@ -16,17 +16,6 @@ function HomePage() {
     </div>
   )
 }
-
-/*function LoginPage(){
-  return(
-    <div>
-      <input placeholder='Benutzername'type='text' style={{marginBottom: '1rem'}}/>
-      <input placeholder='Passwort'type='password' style={{marginBottom: '1rem'}}/>
-      <br/>
-      <button>Login</button>
-    </div>
-  )
-}*/
 
 function AboutPage() {
   return (
@@ -77,17 +66,21 @@ function App() {
   return (
     <div>
       <nav style={{ marginBottom: '1rem' }}>
+        <Link to="/help" style={{ marginRight: '1rem' }}><img className='fragezeichen' src="src/bilder/fragezeichen.svg" ></img></Link>
         <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
-        <Link to="/about" style={{ marginRight: '1rem' }}>About</Link>
-        <Link to="/api-test" style={{ marginRight: '1rem' }}>API Test</Link>
-        <Link to="/login">Login</Link>
+        {/*<Link to="/about" style={{ marginRight: '1rem' }}>About</Link>
+        <Link to="/api-test" style={{ marginRight: '1rem' }}>API Test</Link>*/}
+        <Link to="/login" style={{ marginRight: '1rem' }}>Login</Link>
+        <Link to="/main" style={{ marginRight: '1rem' }}>Hauptansicht</Link>
+        <Link to="/settings"><button style={{"font-size":20, "padding":"0.2em 0.5em"}}>⚙</button></Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/api-test" element={<ApiTestPage />} />
+        {/*<Route path="/about" element={<AboutPage />} />
+        <Route path="/api-test" element={<ApiTestPage />} />*/}
         <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/main" element={<MainPage/>}/>
       </Routes>
     </div>
   )
---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,5 @@
+import { Routes, Route, Link } from 'react-router-dom'
+function MainPage(){
+    
+}
+export default MainPage
