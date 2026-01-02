f47106f0560a0f9c54118d31f90aa904d6fe5d34
Jokras <65406689+1jokras@users.noreply.github.com>
2026-01-02 14:36:38 +0100

    Add statistics presets and export URL to API interface
    Extended the IApiCaller interface and MockApiCaller class to support fetching statistics presets and generating export URLs. Updated StatisticsPage to dynamically load presets from the API and improved select field alignment.

--- frontend/src/classes/IApiCaller.tsx
+++ frontend/src/classes/IApiCaller.tsx
@@ -1,5 +1,13 @@
 export interface IApiCaller {
   GetUsers(): string[];
+  GetExportUrl(
+    timeStart: string,
+    timeEnd: string,
+    preset: string,
+    presetOverrides: string,
+    fileformat: string,
+  ): string;
+  GetStatisticsPresets(): string[];
 }
 
 export class MockApiCaller implements IApiCaller {
@@ -8,5 +16,23 @@ export class MockApiCaller implements IApiCaller {
   GetUsers(): string[] {
     //check current user rights -> done in backend
     return this.users;
+  }
+
+  GetStatisticsPresets(): string[] {
+    return [
+      "Statische Angaben zu den Fachberatungstellen Sexulaisierte Gewalt",
+      "Alle Statisische Angaben die Wir haben",
+      "Preset 3",
+    ];
+  }
+
+  GetExportUrl(
+    timeStart: string,
+    timeEnd: string,
+    preset: string,
+    presetOverrides: string,
+    fileformat: string,
+  ): string {
+    return "/test.csv";
   }
 }
--- frontend/src/pages/StatisticsPage.tsx
+++ frontend/src/pages/StatisticsPage.tsx
@@ -5,8 +5,11 @@ import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import MenuItem from "@mui/material/MenuItem";
+import { MockApiCaller } from "../classes/IApiCaller";
 
 function StatisticsPage() {
+  const presets = new MockApiCaller().GetStatisticsPresets();
+
   return (
     <Box>
       <Grid container spacing={2}>
@@ -18,20 +21,30 @@ function StatisticsPage() {
                 <DatePicker label="Bis" />
               </Stack>
               <Stack spacing={2} direction="row">
-                <TextField select fullWidth label="Vorlage">
-                  <MenuItem>
-                    Statische Angaben zu den Fachberatungstellen Sexulaisierte
-                    Gewalt
-                  </MenuItem>
-                  <MenuItem>Alle Statisische Angaben die Wir haben</MenuItem>
+                <TextField
+                  select
+                  fullWidth
+                  label="Vorlage"
+                  sx={{ "& .MuiSelect-select": { textAlign: "left" } }}
+                >
+                  {presets.map((preset) => (
+                    <MenuItem key={preset} value={preset}>
+                      {preset}
+                    </MenuItem>
+                  ))}
                 </TextField>
                 <Button variant="contained" size="large" sx={{ px: 3 }}>
                   Berechnen
                 </Button>
               </Stack>
               <Stack spacing={2} direction="row">
-                <TextField select fullWidth label="Format">
-                  <MenuItem>CSV</MenuItem>
+                <TextField
+                  select
+                  fullWidth
+                  label="Format"
+                  sx={{ "& .MuiSelect-select": { textAlign: "left" } }}
+                >
+                  <MenuItem value="CSV">CSV</MenuItem>
                 </TextField>
                 <Button variant="outlined" size="large" sx={{ px: 3 }}>
                   Exportieren
