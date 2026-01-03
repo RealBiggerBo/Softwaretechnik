1976928dc3bccb9d5eb94dacce11d82f68e70f00
Globus-I <239416356+Globus-I@users.noreply.github.com>
2026-01-03 17:44:51 +0100

    Erweiterung von Anfrage und Fall

--- backend/api/database/models.py
+++ backend/api/database/models.py
@@ -3,72 +3,88 @@ from datetime import date
 
 #Aufzählungen für Wahl-Felder
 class Beratungsstelle(models.TextChoices):
-    STADT = "S", "Stadt Leipzig"
-    LAND = "L", "Landkreis Leipzig"
-    NORDSACHSEN = "N", "Landkreis Nordsachsen"
+    S = "S", "Stadt Leipzig"
+    L = "L", "Landkreis Leipzig"
+    N = "N", "Landkreis Nordsachsen"
 
 class Ort(models.TextChoices):
-    STADT = "S", "Stadt Leipzig"
-    LAND = "L", "Landkreis Leipzig"
-    NORDSACHSEN = "N", "Landkreis Nordsachsen"
-    SACHSEN = "B", "Sachsen"
-    ANDERES = "A", "Anderes"
+    S = "S", "Stadt Leipzig"
+    L = "L", "Landkreis Leipzig"
+    N = "N", "Landkreis Nordsachsen"
+    B = "B", "Sachsen"
+    X = "X", "Sonstiges"
+    #Es fehlt noch eine weitere Auswahl
 
-class RolleAnfrage(models.TextChoices):
-    FACHKRAFT = "F", "Fachkraft"
-    ANGEHÖRIG = "A", "Angehörige:r"
-    BETROFFEN = "B", "Betroffene:r"
-    ANONYM = "U", "anonym"
-    BETROFFEN_QUEER = "qB", "queer Betroffene:r"
-    FACHKRAFT_QUEER = "qF", "queer Fachkraft"
-    ANGEHÖRIG_QUEER = "qA", "queer Angehörige:r"
-    ANONYM_QUEER = "qU", "queer anonym"
-    FACHKRAFT_FÜR_BETROFFEN = "FfB", "Fachkraft für Betroffene:r"
-    ANGEHÖRIG_FÜR_BETROFFEN = "AfB", "Angehörige:r für Betroffene:r"
-    FACHKRAFT_FÜR_BETROFFEN_QUEER = "FfqB", "Fachkraft für queere Betroffene:r"
-    ANGEHÖRIG_FÜR_BETROFFEN_QUEER = "AfqB", "Angehörige:r für queere Betroffene:r"
+class Rolle(models.TextChoices):
+    B = "B", "Betroffene:r"
+    F = "F", "Fachkraft"
+    A = "A", "Angehörige:r"
+    U = "U", "anonym"
 
-class ArtAnfrage(models.TextChoices):
-    MEDIZINISCHE_HILFE = "MSH", "medizinische Soforthilfe"
-    SPURENSICHERUNG = "VSS", "Vertrauliche Spurensicherung"
-    BERATUNG = "B", "Beratungsbedarf"
-    BERATUNG_RECHT = "BR", "Beratungsbedarf zu Rechtlichem"
-    SONSTIGES = "S", "Sonstiges"
+class AnfrageArt(models.TextChoices):
+    M = "M", "medizinische Soforthilfe"
+    S = "S", "Vertrauliche Spurensicherung"
+    B = "B", "Beratungsbedarf"
+    R = "R", "Beratungsbedarf zu Rechtlichem"
+    X = "X", "Sonstiges"
 
-class RolleFall(models.TextChoices):
-    BETROFFEN = "B", "Betroffene:r"
-    ANGEHÖRIG = "A", "Angehörige:r"
-    FACHKRAFT = "F", "Fachkraft"
+class Geschlecht(models.TextChoices):
+    C = "C", "cis weiblich"
+    T = "T", "trans weiblich"
+    M = "M", "trans männlich"
+    N = "N", "trans nicht binär"
+    I = "I", "inter"
+    A = "A", "agender"
+    D = "D", "divers"
+
+class Sexualitaet(models.TextChoices):
+    L = "L", "lesbisch"
+    S = "S", "schwul"
+    B = "B", "bisexuell"
+    A = "A", "asexuell"
+    H = "H", "heterosexuell"
+
+class BeruflicheSituation(models.TextChoices):
+    L = "L", "arbeitslos"
+    S = "S", "studierend"
+    B = "B", "berufstätig"
+    R = "R", "berentet"
+    A = "A", "Azubi"
+    U = "U", "berufsunfähig"
+
+class BehinderungsForm(models.TextChoices):
+    P = "P", "kognitiv"
+    K = "K", "körperlich"
 
 # Create your models here.
 class Anfrage(models.Model):
-    wie = models.CharField(max_length=200)
-    datum = models.DateField(default=date.today)
-    ort = models.CharField(max_length=1, choices=Ort)
-    rolle = models.CharField(max_length=4, choices=RolleAnfrage)
-    art = models.CharField(max_length=3, choices=ArtAnfrage)
-    termin = models.BooleanField()
-    termin_ort = models.CharField(max_length=1, choices=Beratungsstelle)
-    termin_datum = models.DateField(default=date.today)
+    sende_art = models.CharField(max_length=200)
+    sende_datum = models.DateField(default=date.today)
+    sende_ort = models.CharField(max_length=1, choices=Ort)
+    sender_rolle = models.CharField(max_length=1, choices=Rolle)
+    im_auftrag = models.BooleanField()
+    ist_queer = models.BooleanField()
+    anfrage_art = models.CharField(max_length=1, choices=AnfrageArt)
+    mit_termin = models.BooleanField()
+    termin_ort = models.CharField(max_length=1, choices=Beratungsstelle, blank=True)
+    termin_datum = models.DateField(blank=True)
 
     def __str__(self):
-        return self.wie
+        return self.sende_art
 
 class Fall(models.Model):
     alias = models.CharField(max_length=50)
-    rolle = models.CharField(max_length=1, choices=RolleFall)
+    rolle = models.CharField(max_length=1, choices=Rolle)
     alter = models.IntegerField(blank=True)
-    # identität = 
-    # sexualität =
-    # wohnort =
-    # staatsangehörigkeit =
-    # beruf = 
-    # schwerbehinderung =
-    # schwerbehinderung_form =
-    # schwerbehinderung_grad = 
-    # notizen =
+    geschlecht = models.CharField(max_length=1, choices=Geschlecht, blank=True)
+    sexualitaet = models.CharField(max_length=1, choices=Sexualitaet, blank=True)
+    wohnort = models.CharField(max_length=1, choices=Ort, blank=True)
+    staatsangehoerigkeit = models.CharField(max_length=1, choices=Ort)
+    berufssituation = models.CharField(max_length=1, choices=BeruflicheSituation, blank=True)
+    schwerbehinderung = models.BooleanField()
+    schwerbehinderung_form = models.CharField(max_length=1, choices=BehinderungsForm)
+    schwerbehinderung_grad = models.CharField(max_length=50)
+    notizen = models.CharField(max_length=50)
 
     def __str__(self):
         return self.alias
-
-# year_in_school = models.CharField(max_length=2, choices=YearInSchool, default=YearInSchool.FRESHMAN,)
