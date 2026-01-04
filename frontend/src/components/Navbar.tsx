dfb48dfaacfaac8e8f0f73248126e405d343b020
Lucy <stahr.lucy@gmail.com>
2026-01-04 10:20:02 +0100

    add navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,33 @@
+import { AppBar, Toolbar, Button, Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+
 function Navbar() {
-  return <div style={{ color: "red" }}>Navbar component</div>;
+  const navigate = useNavigate();
+  return (
+        <AppBar position="static">
+      <Toolbar>
+        {/* Linke Seite */}
+        <Button color="inherit" onClick={() => navigate("/help")}>
+          Hilfe
+        </Button>
+
+        {/* Abstand zwischen links und rechts */}
+        <Box sx={{ flexGrow: 1 }} />
+
+        {/* Rechte Seite */}
+        <Button color="inherit" onClick={() => navigate("/logout")}>
+          Logout
+        </Button>
+        <Button color="inherit" onClick={() => navigate("/main")}>
+          Home
+        </Button>
+        <Button color="inherit" onClick={() => navigate("/settings")}>
+          Settings
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Navbar;
