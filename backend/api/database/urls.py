6d6b00e66f96c29588f947e79fbcba45bc13ce32
Globus-I <239416356+Globus-I@users.noreply.github.com>
2026-01-05 19:56:12 +0100

    Aktualisierung der Data API und der entsprechenden Dokumentation. Sowie Änderung des Standard-Tokens zu Testzwecken.

--- backend/api/database/urls.py
+++ backend/api/database/urls.py
@@ -3,12 +3,12 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path("get_list", views.get_list),
-    path("save", views.save),
-    path("get/<int:pk>/", views.get),
-    path("update/<int:pk>/", views.update),
-    path("delete/<int:pk>/", views.delete),
-    path("search", views.search),
+    path("get_list/<str:type>", views.get_list),
+    path("save/<str:type>", views.save),
+    path("get/<str:type>/<int:pk>/", views.get),
+    path("update/<str:type>/<int:pk>/", views.update),
+    path("delete/<str:type>/<int:pk>/", views.delete),
+    path("search/<str:type>", views.search),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -146,7 +146,7 @@ CORS_ALLOW_CREDENTIALS = True
 # Tokenverwaltung 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
