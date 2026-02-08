1cc74f9d445cfb22cc3b8b3ebbaa2dad71a6d2ae
Toaches <142804447+Toaches@users.noreply.github.com>
2026-02-08 10:36:12 +0100

    Remove Tests
    Tests Aufräumen

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path, include
 from .views import hello_world
-from .views_stats import case_stats
-from .views_export import export_cases_csv
 
 urlpatterns = [
     path('hello/', hello_world, name='hello-world'),
-    path('stats/cases/', case_stats, name='case-stats'),
-    path('export/cases/', export_cases_csv, name='case-export'),
     path("stats/", include("api.stats.urls")),
 ]
