e138764c0c561f3ae23a17998fa1a0db70cb0c08
Toaches <142804447+Toaches@users.noreply.github.com>
2026-02-08 10:28:18 +0100

    Delete backend/api/admin.py
    Tests aufräumen

--- backend/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Case
-
-@admin.register(Case)
-class CaseAdmin(admin.ModelAdmin):
-    list_display = ("id", "category", "severity", "victim_age", "victim_gender", "city", "region", "date_occurred", "status")
-    list_filter = ("category", "victim_gender", "region", "status")
-    search_fields = ("city", "region", "notes")
