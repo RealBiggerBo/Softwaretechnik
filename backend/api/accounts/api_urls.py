8d2eeefdaf5c1c94786aaacda2d7520c735edcaa
Yggdraghost <floria.kunze@arcor.de>
2026-02-10 17:45:07 +0100

    überflüssigen code gelöscht

--- backend/api/accounts/api_urls.py
+++ backend/api/accounts/api_urls.py
@@ -5,13 +5,6 @@ from .views import (
     LogoutAPIView, 
     MeAPIView, 
     ChangePasswordAPI,
-    # CreateDatasetAPI,
-    # UpdateDatasetAPI,
-    # StatisticsAPI,
-    # UserPresetAPI,
-    # SharedPresetCreateAPI,
-    # SharedPresetDeleteAPI,
-    # CreateFormFieldAPI,
 )
 from .admin_api import (
     AdminUserRegisterAPI,
@@ -28,15 +21,6 @@ urlpatterns = [
     path('logout/', LogoutAPIView.as_view()),
     path('me/', MeAPIView.as_view()),
     path('change-password/', ChangePasswordAPI.as_view()),
-
-    # Rollen und was sie dürfen
-    # path("datasets/", CreateDatasetAPI.as_view()),
-    # path("datasets/<int:dataset_id>/", UpdateDatasetAPI.as_view()),
-    # path("statistics/", StatisticsAPI.as_view()),
-    # path("presets/", UserPresetAPI.as_view()),
-    # path("presets/shared/", SharedPresetCreateAPI.as_view()),
-    # path("presets/shared/<int:preset_id>/", SharedPresetDeleteAPI.as_view()),
-    # path("form-fields/", CreateFormFieldAPI.as_view()),
 
     # Admin
     path('admin/users/register/', AdminUserRegisterAPI.as_view()),
