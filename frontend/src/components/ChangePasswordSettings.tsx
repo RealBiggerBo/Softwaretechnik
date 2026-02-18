b87f45b067b496a14c48e4f167e3968490fc3548
MissNati01 <ny4mpds6xg@privaterelay.appleid.com>
2026-02-18 22:42:18 +0100

    add snackbar for change password

--- frontend/src/components/ChangePasswordSettings.tsx
+++ frontend/src/components/ChangePasswordSettings.tsx
@@ -1,20 +1,10 @@
 import { useState } from "react";
 import type { IApiCaller } from "../classes/IApiCaller";
 import PasswordInput from "../components/PasswordInput";
+import { Snackbar, Alert } from "@mui/material";
 
 interface Props {
   caller: IApiCaller;
-}
-
-async function submitPasswordChangeRequest(
-  caller: IApiCaller,
-  oldPswd: string,
-  newPswd: string,
-  newPswdCtrl: string,
-): Promise<void> {
-  const result = await caller.TryChangePassword(oldPswd, newPswd, newPswdCtrl);
-  if (!result.success) alert(result.errorMsg);
-  else alert("Passwort erfolgreich geändert");
 }
 
 //Generiert den Change Password bereich in den Einstellungen
@@ -22,6 +12,34 @@ function ChangePasswordSettings({ caller }: Props) {
   const [oldPswd, setOldPswd] = useState("");
   const [newPswd, setNewPswd] = useState("");
   const [newPswdCtrl, setNewPswdCtrl] = useState("");
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const [saveResult, setSaveResult] = useState(false);
+
+  async function submitPasswordChangeRequest(
+    caller: IApiCaller,
+    oldPswd: string,
+    newPswd: string,
+    newPswdCtrl: string,
+  ): Promise<void> {
+    const result = await caller.TryChangePassword(
+      oldPswd,
+      newPswd,
+      newPswdCtrl,
+    );
+    if (!result.success) {
+      activateSnackbar(result.errorMsg);
+      setSaveResult(false);
+    } else {
+      activateSnackbar("Passwort erfolgreich geändert");
+      setSaveResult(true);
+    }
+  }
+
+  function activateSnackbar(msg: string) {
+    setOpenSnackbar(true);
+    setSnackbarMessage(msg);
+  }
 
   return (
     <form
@@ -57,6 +75,18 @@ function ChangePasswordSettings({ caller }: Props) {
       <button id="submit" className="passwordChangeSubmitBtn" type="submit">
         Passwort ändern
       </button>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={3000}
+        onClose={() => setOpenSnackbar(false)}
+      >
+        <Alert
+          severity={saveResult ? "success" : "error"}
+          onClose={() => setOpenSnackbar(false)}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </form>
   );
 }
