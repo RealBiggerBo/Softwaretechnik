cf83339196d78b159a6b8465e526430c4a8d13e6
Sepp Stage <Seppniklas@outlook.de>
2026-02-18 21:22:16 +0100

    ADDED:  -ui for changing required/sensitive

--- frontend/src/components/Fieldrenderer.tsx
+++ frontend/src/components/Fieldrenderer.tsx
@@ -6,6 +6,18 @@ import ToggleDataField from "./ToggleDataField";
 import ListDataField from "./ListDataField";
 import { type DataField } from "../classes/DataField";
 import GroupDataField from "./GroupDataField";
+import {
+  Checkbox,
+  checkboxClasses,
+  Collapse,
+  FormControlLabel,
+  Grid,
+  IconButton,
+  Stack,
+} from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import { useState } from "react";
 
 interface Props {
   field: DataField;
@@ -16,14 +28,14 @@ interface Props {
   setOpenDialog: (showDialog: boolean) => void;
 }
 
-export function FieldRenderer({
-  field,
-  isEditMode,
-  onChange,
-  onDelete,
-  onAdd,
-  setOpenDialog,
-}: Props) {
+function GetFieldComponent(
+  field: DataField,
+  isEditMode: boolean,
+  onChange: (field: DataField) => void,
+  onAdd: (fieldToAdd: DataField) => void,
+  onDelete: (id: number) => void,
+  setOpenDialog: (showDialog: boolean) => void,
+) {
   switch (field.type) {
     case "text":
       return (
@@ -89,7 +101,89 @@ export function FieldRenderer({
           setOpenDialog={setOpenDialog}
         />
       );
+    default:
+      const _exhaustive: never = field;
+      return _exhaustive;
   }
+}
 
-  throw new Error("Unhandled field type");
+function GetFieldCollapse(
+  field: DataField,
+  onChange: (toChange: DataField) => void,
+) {
+  return (
+    <>
+      <Stack spacing={1} sx={{ alignItems: "flex-start" }}>
+        <FormControlLabel
+          checked={field.required}
+          control={
+            <Checkbox
+              onChange={(selected) =>
+                onChange({
+                  ...field,
+                  required: selected.target.checked,
+                })
+              }
+            />
+          }
+          label="Pflichtfeld"
+          labelPlacement="start"
+        />
+        <FormControlLabel
+          checked={field.sensitive}
+          control={
+            <Checkbox
+              onChange={(selected) =>
+                onChange({ ...field, sensitive: selected.target.checked })
+              }
+            />
+          }
+          label="Sensibel"
+          labelPlacement="start"
+        />
+      </Stack>
+    </>
+  );
+}
+
+export function FieldRenderer({
+  field,
+  isEditMode,
+  onChange,
+  onAdd,
+  onDelete,
+  setOpenDialog,
+}: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        {isEditMode && (
+          <IconButton
+            onClick={() => {
+              if (isEditMode) setIsOpen(!isOpen);
+            }}
+          >
+            {isOpen && isEditMode ? (
+              <KeyboardArrowUpIcon />
+            ) : (
+              <KeyboardArrowDownIcon />
+            )}
+          </IconButton>
+        )}
+        {GetFieldComponent(
+          field,
+          isEditMode,
+          onChange,
+          onAdd,
+          onDelete,
+          setOpenDialog,
+        )}
+      </div>
+      <Collapse in={isOpen && isEditMode}>
+        {GetFieldCollapse(field, onChange)}
+      </Collapse>
+    </>
+  );
 }
