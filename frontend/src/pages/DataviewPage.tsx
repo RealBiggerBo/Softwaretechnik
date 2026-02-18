22ced4f242ab07ee2357e33cf02f69814b3a81e1
MissNati01 <ny4mpds6xg@privaterelay.appleid.com>
2026-02-18 17:11:46 +0100

    delete import

--- frontend/src/pages/DataviewPage.tsx
+++ frontend/src/pages/DataviewPage.tsx
@@ -1,9 +1,5 @@
 import { useSearchParams } from "react-router-dom";
 import type { IApiCaller } from "../classes/IApiCaller";
-import AnfragenGenerator from "../components/AnfragenGenerator";
-import FallGenerator from "../components/FallGenerator";
-import LetzteAnfrage from "../components/LetzeAnfrage";
-import LetzterFall from "../components/LetzterFall";
 import DataRecordEditor from "../components/DataRecordEditor";
 
 interface Props {
