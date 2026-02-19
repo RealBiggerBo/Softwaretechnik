58cd259df987dc3adf0853faa0a412aebb553eb3
Sepp Stage <Seppniklas@outlook.de>
2026-02-19 14:59:55 +0100

    CHANGED:  -FilterOptionDisplay style

--- frontend/src/components/FilterOptionList.tsx
+++ frontend/src/components/FilterOptionList.tsx
@@ -3,6 +3,8 @@ import type { UiItem } from "../classes/UiItems";
 import FilterOptionDisplay from "./FilterOptionDisplay";
 import type { FilterOption } from "../classes/FilterOption";
 import StyledButton from "./Styledbutton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Box, Fab, Icon, IconButton, Paper, Stack } from "@mui/material";
 
 interface Props {
   filterOptions: UiItem<FilterOption>[];
@@ -29,19 +31,48 @@ function FilterOptionList({
   return (
     <>
       {filterOptions.map((option, _) => (
-        <div key={option.id}>
-          <FilterOptionDisplay
-            option={option}
-            format={format}
-            onChange={(newOption) => {
-              updateFilterOption(option, newOption);
+        <Paper elevation={5} sx={{ padding: "7px", marginTop: "10px" }}>
+          <Stack
+            spacing={0.5}
+            direction="row"
+            sx={{
+              alignItems: "flex-start",
+              justifyContent: "space-between",
+              width: "100%",
             }}
-          ></FilterOptionDisplay>
-          <StyledButton
-            text={removeText}
-            onClick={() => removeFilterOption(option)}
-          />
-        </div>
+            key={option.id}
+          >
+            <Box sx={{ flexGrow: 1 }}>
+              <FilterOptionDisplay
+                option={option}
+                format={format}
+                onChange={(newOption) => {
+                  updateFilterOption(option, newOption);
+                }}
+              ></FilterOptionDisplay>
+            </Box>
+            <Box
+              sx={{
+                height: 56, // height of one autocomplete
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <Fab
+                color="error"
+                aria-label={removeText}
+                onClick={() => removeFilterOption(option)}
+              >
+                <DeleteIcon />
+              </Fab>
+              {/* <StyledButton
+                text={removeText}
+                color="error"
+                onClick={() => removeFilterOption(option)}
+              /> */}
+            </Box>
+          </Stack>
+        </Paper>
       ))}
       <br></br>
       <StyledButton text={addText} onClick={() => addNewFilterOption()} />
